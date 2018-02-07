e30d39b9ce93897b025f54466906e066f397df97
兰兄 <lanxiong@lanxiongdeiMac.local>
2018-02-07 17:55:51 +0800

    two commit

--- index.js
+++ index.js
@@ -24,16 +24,20 @@ var titles = [];     //放置数据
 
 var URL = 'http://www.ygdy8.net'    //详情页链接
 
+var superagent = require('superagent')
+
 function btLink (i,n){
     http.get(`${URL}${n[i-1].href}`,(sres)=>{
+        sres.setEncoding('utf-8');
         var thunks = [];
         console.log('正在爬取第'+i+"个种子")
         sres.on('data',(thunk)=>{                 //同下边一样
             thunks.push(thunk)
         })
-        sres.on('end',()=>{
-            var html = iconv.decode(Buffer.concat(thunks),'gb2312')
-            var $ = cheerio.load(html,{decodeEntities:false});
+        sres.on('end',(res)=>{
+
+            //var html = iconv.decode(Buffer.concat(thunks),'gb2312')
+            var $ = cheerio.load(thunks,{decodeEntities:false});
             $("#Zoom td").children('a').each((index,item)=>{
                 var $item = $(item)
                 titles[i-1].bt = $item.attr('href')
@@ -52,7 +56,7 @@ let save = (titles) => {
     titles.forEach((m,i)=>{      //遍历将每一条数据都保存到数据库
         Mongoclient.connect(URLS,(err,db)=>{
             assert.equal(null,err)
-            db.collection('video').insert({video_name:m.title,bt:m.bt,id:i+1},(err,result)=>{
+            db.collection('video2').insert({video_name:m.title,bt:m.bt,id:i+1},(err,result)=>{
                 if (err) console.log(err)
                 db.close()
             })
@@ -64,7 +68,7 @@ let save = (titles) => {
 let video = (i,url)=>{
     var u = `${url}${i}.html`    //爬取的url
     http.get(u,(sres)=>{        //先向页面发起第一次请求
-        console.log("正在爬取"+i+'页')
+        console.log("正在爬取第"+i+'页')
         var thunks = [];        //放置html代码
         sres.on('data',(thunk)=>{     //监听data并将html代码放在数组中
             thunks.push(thunk)
@@ -79,7 +83,7 @@ let video = (i,url)=>{
                     href:$item.attr("href")      //详情页的地址
                 });
             })
-            if (i === 1){                        //想要爬取的页数
+            if (i === 10){                        //想要爬取的页数
                 console.log(titles)              //递归调用   判断如果没有爬取完毕  接着调用自身
                 console.log("爬取完毕")             //如果爬取完毕   继续爬取种子
                 console.log('开始爬取种子')
@@ -91,9 +95,26 @@ let video = (i,url)=>{
     })
 }
 
+let music = (url)=>{
+    var music = []
+    superagent.get(url).end((err,res)=>{
+        if (err) {
+            return err
+        }
+        var $ = cheerio.load(res.text);
+        $('.col5 .icon-play a').each((index,item)=>{
+            var $item = $(item)
+            music.push({name:$item.text()})
+        })
+        console.log(music)
+    })
+}
+
+
 let main = (i,url)=>{
     console.log('开始爬取')
-    video(i,url)   //先爬取页数
+    //video(i,url)   //先爬取页数
+    music('https://music.douban.com/chart')
 }
 
 main(index,url)
